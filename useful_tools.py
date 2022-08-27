44b06e5c10acec05825640a7fbe2d0c3fe84beee
Xiao Huang <97466269+XiaoHuangMIT@users.noreply.github.com>
2022-08-27 10:21:58 -0400

    Update useful_tools.py

--- useful_tools.py
+++ useful_tools.py
@@ -3,27 +3,27 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-def separate_metal_and_charge(df, count_only == False):
+def separate_metal_and_charge(df, count_only = False):
+    
+    dffe = df[df.metal == 'Fe']
+    dffe2 = dffe[dffe.ox_csd == 2]
+    dffe3 = dffe[dffe.ox_csd == 3]
+    
+    dfco = df[df.metal == 'Co']
+    dfco2 = dfco[dfco.ox_csd == 2]
+    dfco3 = dfco[dfco.ox_csd == 3]
+    
+    dfmn = df[df.metal == 'Mn']
+    dfmn2 = dfmn[dfmn.ox_csd == 2]
+    
+    lcount = []
+    lcount.append(dffe2.shape[0])
+    lcount.append(dffe3.shape[0])
+    lcount.append(dfco2.shape[0])
+    lcount.append(dfco3.shape[0])
+    lcount.append(dfmn2.shape[0])
   
-  dffe = df[df.metal == 'Fe']
-  dffe2 = dffe[dffe.ox_csd == 2]
-  dffe3 = dffe[dffe.ox_csd == 3]
-
-  dfco = df[df.metal == 'Co']
-  dfco2 = dfco[dfco.ox_csd == 2]
-  dfco3 = dfco[dfco.ox_csd == 3]
-
-  dfmn = df[df.metal == 'Mn']
-  dfmn2 = dfmn[dfmn.ox_csd == 2]
-  
-  lcount = []
-  lcount.append(dffe2.shape[0])
-  lcount.append(dffe3.shape[0])
-  lcount.append(dfco2.shape[0])
-  lcount.append(dfco3.shape[0])
-  lcount.append(dfmn2.shape[0])
-  
-  if count_only == True:
-    return lcount
-  else:
-    return dffe2, dffe3, dfco2, dfco3, dfmn2, lcount
+    if count_only == True:
+        return lcount
+    else: 
+        return dffe2, dffe3, dfco2, dfco3, dfmn2, lcount
