4d9326d606b3e6c3cf9195f9277a7844695434cf
Xiao Huang <97466269+XiaoHuangMIT@users.noreply.github.com>
2022-08-27 10:16:42 -0400

    Update useful_tools.py

--- useful_tools.py
+++ useful_tools.py
@@ -1,3 +1,19 @@
 import numpy as np 
 import pandas as pd
 import matplotlib.pyplot as plt
+
+
+def separate_metal_and_charge(df):
+  
+  dffe = df[df.metal == 'Fe']
+  dffe2 = dffe[dffe.ox_csd == 2]
+  dffe3 = dffe[dffe.ox_csd == 3]
+
+  dfco = df[df.metal == 'Co']
+  dfco2 = dfco[dfco.ox_csd == 2]
+  dfco3 = dfco[dfco.ox_csd == 3]
+
+  dfmn = df[df.metal == 'Mn']
+  dfmn2 = dfmn[dfmn.ox_csd == 2]
+  
+  return dffe2,dffe3,dfco2,dfco3,dfmn2
