0e69ef1aeaaedd49fdd209dc6fd4d2fe0c6c8d6c
Xiao Huang <97466269+XiaoHuangMIT@users.noreply.github.com>
2022-09-15 14:43:26 -0400

    Update cogef.py

--- cogef.py
+++ cogef.py
@@ -2,6 +2,7 @@ import os
 import sys
 import numpy as np
 import pandas as pd
+from os.path import exists
 from molSimplify.Classes.mol3D import mol3D
 from molSimplify.Classes.ligand import ligand_breakdown
 
@@ -79,3 +80,76 @@ def calculate_force(Es,dist=0.2):
         F = F*1.66/100 #in nN
         Fs.append(F)
     return max(Fs)
+
+
+
+def continue_COGEF(oldpath,new_parent_path,walltime = 'same',stretch_dist = 10, dist_step = 0.2):
+    
+    #Generate new directory, input, jobscript and xyz with same name in the new parent folder
+    #So that job manager could be run in the new parent path without re-running finished jobs
+    #Input will be modified as continuing of COGEF
+    #Tip: cannot change strech distance or step distance for a job that starts from scratch
+    
+    #Read scan_optim
+    from_scratch = False
+    old_scan_path = oldpath + '/scr/scan_optim.xyz'
+    if exists(old_scan_path) == False or os.stat(old_scan_path).st_size == 0:
+        from_scratch = True
+    else:
+        old_scan_file = open(old_scan_path, 'r')
+        old_scan_lines = old_scan_file.readlines()
+        distances = []
+        for line in old_scan_lines:
+            if 'Converged' in line:
+                distance = line.split()[6]
+                distance = distance.replace('(','')
+                distance = distance.replace(')','')
+                distances.append(float(distance))
+        d0,dt = distances[0], distances[-1] #beginning frame and last frame finished
+    
+    #Make directory for continued COGEF run
+    new_path = new_parent_path + '/' + oldpath
+    if os.path.exists(new_parent_path) == False: #make things easier
+        os.mkdir(new_parent_path)
+    if os.path.exists(new_path) == False:
+        os.mkdir(new_path)
+    
+    #Write jobscript
+    old_js = open(oldpath + '/' + oldpath + '_jobscript')
+    js_lines = old_js.readlines()
+    if walltime != 'same': #if we want to change walltime
+        for idx in np.arange(len(js_lines)):
+            if '#$ -l h_rt' in js_lines[idx]:
+                js_lines[idx] = '#$ -l h_rt' + ' '+ walltime + '\n'
+    new_js = new_path + '/' + oldpath + '_jobscript'
+    with open(new_js, 'w') as f:
+        for line in js_lines:
+            f.write(str(line))
+        f.close()
+    
+    #Write input
+    if from_scratch == False:
+        dini, dfin = dt + dist_step, d0 + stretch_dist #beginning and end point for new COGEF
+        nsteps = ((dfin - dini) / dist_step) + 1
+        nsteps = int(nsteps)
+    old_in = open(oldpath + '/' + oldpath + '.in')
+    in_lines = old_in.readlines()
+    if from_scratch == False:
+        for idx in np.arange(len(in_lines)):
+            if 'bond' in in_lines[idx]:
+                pp_pair = in_lines[idx].split()[-1]
+                in_lines[idx] = 'bond ' + str(dini) + ' ' + str(dfin) + ' ' + str(nsteps) + ' ' + pp_pair + '\n'
+    new_in = new_path + '/' + oldpath + '.in'
+    with open(new_in, 'w') as f:
+        for line in in_lines:
+            f.write(str(line))
+        f.close()
+    
+    #Write(Copy) xyz
+    old_xyz = open(oldpath + '/' + oldpath + '.xyz')
+    xyz_lines = old_xyz.readlines()
+    new_xyz = new_path + '/' + oldpath + '.xyz'
+    with open(new_xyz,'w') as f:
+        for line in xyz_lines:
+            f.write(str(line))
+        f.close()
