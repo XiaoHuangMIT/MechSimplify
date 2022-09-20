49e85f0aa1267d67d128462a50ded29c51eada7c
Xiao Huang <97466269+XiaoHuangMIT@users.noreply.github.com>
2022-09-20 13:35:37 -0400

    Update coord_analysis.py

--- coord_analysis.py
+++ coord_analysis.py
@@ -9,6 +9,8 @@ from molSimplify.Classes.ligand import ligand_breakdown
 from molSimplify.Scripts.geometry import vecangle
 import networkx as nx
 
+
+
 def find_central_atom(molecule):
   
     #Returns the central coordinating atom of two ligands
@@ -36,3 +38,38 @@ def find_central_atom(molecule):
       
     return ccaid1,ccaid2
   
+  
+  
+def find_dcc(molecule,ccaid=None):
+    
+    #Analyzing distances between coordinating atoms
+    #Returns a list of list, element in format [idx1,idx2,distance]
+    #Analysis based on one ligand only since homoleptic
+    #If mer complex: first two elements correspond to distance between central and two other coordinating atoms
+    #Inputs:
+    #molecule: mol3D object
+    #ccaid one of the central coordinating atoms, default None for fac complexes
+    
+    if ccaid != None: #Mer complexes
+      metal_id = molecule.findMetal()[0]
+      lig1caid,lig2caid = ligand_breakdown(molecule)[2][0],ligand_breakdown(molecule)[2][1]
+      if ccaid in lig1caid:
+        idxs = lig1caid #indexes of coordinating atoms
+      else:
+        idxs = lig2caid
+      idxs.remove(ccaid)
+      idx1, idx2, idx3 = ccaid,idxs[0],idxs[1] #central coordinating atom first
+    else: #Fac complexes
+      idxs = ligand_breakdown(molcule)[2][0]
+      idx1, idx2, idx3 = idxs[0], idxs[1], idxs[2]
+      
+    atom1 = molecule.getAtom(idx1)
+    atom2 = molecule.getAtom(idx2)
+    atom3 = molecule.getAtom(idx3)
+    dcc1 = atom1.distance(atom2)
+    dcc2 = atom1.distance(atom3)
+    dcc3 = atom2.distance(atom3)
+
+    return [[idx1,idx2,dcc1],[idx1,idx3,dcc2],[idx2,idx3,dcc3]]
+
+def 
