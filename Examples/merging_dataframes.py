225a30a229434e0c1e08a882d9407923e606b31c
Xiao Huang <97466269+XiaoHuangMIT@users.noreply.github.com>
2023-01-22 16:52:11 -0500

    Update merging_dataframes.py

--- Examples/merging_dataframes.py
+++ Examples/merging_dataframes.py
@@ -14,3 +14,8 @@ df = pd.merge(df,df_20,how='outer',on='refcode')
 df = pd.merge(df,df_20nd,how='outer',on='refcode')
 df = pd.merge(df,df_15t,how='outer',on='refcode')
 df = pd.merge(df,df_15vvt,how='outer',on='refcode')
+
+
+df1 = pd.read_csv('round1_e_finished.csv')
+df2 = pd.read_csv('round1_scre_finished.csv')
+df = pd.concat([df1,df2],axis=0)
