e2c5a62ff8d759378b5d3f2c710eabfc8088b094
Xiao Huang <97466269+XiaoHuangMIT@users.noreply.github.com>
2023-01-24 14:44:46 -0500

    Update merging_dataframes.py

--- Examples/merging_dataframes.py
+++ Examples/merging_dataframes.py
@@ -19,3 +19,4 @@ df = pd.merge(df,df_15vvt,how='outer',on='refcode')
 df1 = pd.read_csv('round1_e_finished.csv')
 df2 = pd.read_csv('round1_scre_finished.csv')
 df = pd.concat([df1,df2],axis=0)
+#Combine DataFrame objects with overlapping columns and return everything. Columns outside the intersection will be filled with NaN values
