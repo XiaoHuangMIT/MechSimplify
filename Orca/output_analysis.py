735b5320dee73e17c33c6307c3e436c8a99f8486
Xiao Huang <97466269+XiaoHuangMIT@users.noreply.github.com>
2023-01-24 19:32:04 -0500

    Update output_analysis.py

--- Orca/output_analysis.py
+++ Orca/output_analysis.py
@@ -82,13 +82,13 @@ def make_dir(dirname):
      
     
 def find_opt_frames(filepath,natoms):
-    
+
     if os.path.exists(filepath) == False:
         return 'Failed'
 
     lines = open(filepath,'r').readlines()
     xyzs = []
-    for linenum in np.arange(len(lines)): 
+    for linenum in np.arange(len(lines)):
         if 'CARTESIAN COORDINATES (ANGSTROEM)' in lines[linenum]: #xyz beginning two lines later
             firstline = linenum + 2
             lastline = firstline + natoms - 1
@@ -97,39 +97,39 @@ def find_opt_frames(filepath,natoms):
                 xyz.append(lines[i].lstrip())
             xyzs.append(xyz)
     xyzs.pop()#n cycles: n+1 structures, so drop last one
-    
+
     return xyzs
 
 
 
 def check_diss_by_out(basename,threshold = 10,return_list = False):
-    
+
     natoms = open(basename + '/' + basename + '.xyz','r').readlines()[0].split()[0]
     natoms = int(natoms)
-    
+
     frames = find_opt_frames(basename + '/' + basename + '.out', natoms)
     if threshold > len(frames):
         return False
-    
+
     checks = []
     nframes = len(frames)
     first_frame = nframes - threshold + 1
-    
+
     for i in np.arange(first_frame,nframes+1):
         file = open('temp_n.xyz', 'w')
         file.writelines(str(natoms) + '\n')
         file.writelines('\n')
         file.writelines(frames[i-1])
         file.close()
-        
+
         mol = mol3D()
         mol.readfromxyz('temp_n.xyz')
         l1,l2,l3 = ligand_breakdown(mol)
         check = 'intact'
         if l2 != [3,3]:
             check = 'diss'
-        checks.append(check) 
-    
+        checks.append(check)
+
     result = True
     for check in checks:
         if check == 'intact':
@@ -160,7 +160,7 @@ def analyze_efei_expanse(basename, thres=10, record_failed_mol2 = True):
     natoms = open(basename + '/' + basename + '.xyz','r').readlines()[0].split()[0]
     natoms = int(natoms)
     frames = find_opt_frames(basename + '/' + basename + '.out',natoms)
-    if len(frames) == 1:
+    if len(frames) < 2:
         mol2 = 'no_progress'
     else:
         file = open('temp_rec.xyz', 'w')
