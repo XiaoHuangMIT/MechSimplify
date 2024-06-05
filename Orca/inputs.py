79955df867c7aa3b7afdb297421945fc65b0226e
Xiao Huang <97466269+XiaoHuangMIT@users.noreply.github.com>
2024-06-05 13:13:48 -0400

    Update inputs.py

--- Orca/inputs.py
+++ Orca/inputs.py
@@ -115,8 +115,8 @@ def prep_orca_input(refcode, charge, spin, spinval, mol2, machine, metal = None,
             f.write('export OMP_NUM_THREADS=8\n')
             f.write('\n')
             f.write('/home/gridsan/xiaohuang/orca/orca ' + basename + '.in > $SLURM_SUBMIT_DIR/' + basename + '.out\n')
-            #mv *.gbw $SLURM_SUBMIT_DIR/scr
-            #mv *.xyz $SLURM_SUBMIT_DIR/scr
+            f.write('mv *.gbw $SLURM_SUBMIT_DIR/scr\n')
+            f.write('mv *.xyz $SLURM_SUBMIT_DIR/scr\n')
             f.write('rm *.tmp\n')
             f.write('\n')
            
